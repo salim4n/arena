4d61488798e3e11fdb148b05149df96b9f3fd245
Salim <laimeche160@gmail.com>
2022-06-29 21:26:37 +0200

    Test

--- Gladiateur.ts
+++ Gladiateur.ts
@@ -118,6 +118,23 @@ class Ludus {
 }
 
 
+let spartacus = new Mirmillon(200, 100, "Spartacus", new Armement());
+let julius = new Retiaire(150, 150, "Julius", new Armement());
+let caesar = new Berseker(100, 200, "Caesar", new Armement());
+let batiatusLudus = new Ludus("Ludus");
+batiatusLudus.ajouterGladiateur(spartacus);
+batiatusLudus.ajouterGladiateur(julius);
+batiatusLudus.ajouterGladiateur(caesar);
+console.log(batiatusLudus);
+batiatusLudus.retirerGladiateur(spartacus);
+console.log(batiatusLudus);
+batiatusLudus.afficherGladiateurs();
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # arena
+
 - creation d'un jeu de gladiateur en typescript jouable via navigateur (I hope)
 
